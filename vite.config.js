c1a34449697b4aac9893386a19a71574d2cc2288
Seb Smith <78294619+horizonmode@users.noreply.github.com>
2026-09-13 11:42:36 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    rollupOptions: {
-      input: {
-        player: resolve(process.cwd(), "index.html"),
-        presenter: resolve(process.cwd(), "race.html")
-      }
+    plugins: [svelte()],
+    build: {
+        rollupOptions: {
+            input: {
+                player: resolve(process.cwd(), "index.html"),
+                presenter: resolve(process.cwd(), "race.html")
+            }
+        }
     }
-  }
 });
